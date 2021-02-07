2cc30b1af86e03cda1e36404436c31f3fe3bfc8c
Ebenezer Don <ebenezerdonu@gmail.com>
2021-02-07 04:48:14 +0100

    set up app template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 className="green-text center-align">Market List App</h3>
+      <form className="add-item-form">
+        <input type="text" placeholder="Name of item" required />
+        <input type="number" placeholder="Price in USD" required />
+        <button type="submit" className="waves-effect waves-light btn right">Add item</button>
+      </form>
+      <div className="card white darken-1">
+        <div className="card-content">
+          <form action="#">
+            <div className="row">
+              <p className="col s5">
+                <label>
+                  <input type="checkbox" />
+                  <span className="black-text">Tomato</span>
+                </label>
+              </p>
+              <p className="col s5">$5</p>
+              <i className="col s2 material-icons delete-button">delete</i>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
